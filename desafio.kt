0c5c0599596e93b906a08b6577b256aebb20c54e
Anara <128007362+Anara-carolina@users.noreply.github.com>
2023-10-11 20:05:16 -0300

    entrega do projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,52 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+class Formacao(
+    val nome: String,
+    val nivel: String,
+    val conteudosEducacionais: List<ConteudoEducacional>
+) {
+    val alunosMatriculados = mutableListOf<Aluno>()
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricularAluno(aluno: Aluno) {
+        alunosMatriculados.add(aluno)
     }
 }
 
+class ConteudoEducacional(
+    val titulo: String,
+    val descricao: String
+)
+
+class Aluno(
+    val nome: String,
+    val email: String
+)
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando conteúdos educacionais
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", "Aprenda o básico do Kotlin.")
+    val conteudo2 = ConteudoEducacional("Programação Android com Kotlin", "Desenvolva apps Android com Kotlin.")
+
+    // Criando uma formação e adicionando conteúdos educacionais
+    val formacao = Formacao("Formação Android com Kotlin", "Avançado", listOf(conteudo1, conteudo2))
+
+    // Criando alunos
+    val aluno1 = Aluno("João", "joao@email.com")
+    val aluno2 = Aluno("Maria", "maria@email.com")
+
+    // Matriculando alunos na formação
+    formacao.matricularAluno(aluno1)
+    formacao.matricularAluno(aluno2)
+
+    // Imprimindo informações da formação e dos alunos matriculados
+    println("Formação: ${formacao.nome}")
+    println("Nível: ${formacao.nivel}")
+    println("Conteúdos Educacionais:")
+    for (conteudo in formacao.conteudosEducacionais) {
+        println("- ${conteudo.titulo}: ${conteudo.descricao}")
+    }
+
+    println("Alunos Matriculados:")
+    for (aluno in formacao.alunosMatriculados) {
+        println("- Nome: ${aluno.nome}, Email: ${aluno.email}")
+    }
 }
+
